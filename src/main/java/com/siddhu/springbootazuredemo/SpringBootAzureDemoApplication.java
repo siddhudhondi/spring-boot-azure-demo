7f305c351c921ceb85468a1b2921424ef98fd6cc
siddhu <siddhulal01@gmail.com>
2023-01-03 11:03:34 +0530

    master

---
+++ src/main/java/com/siddhu/springbootazuredemo/SpringBootAzureDemoApplication.java
@@ -0,0 +1,13 @@
+package com.siddhu.springbootazuredemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootAzureDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootAzureDemoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/siddhu/springbootazuredemo/SpringBootAzureDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.siddhu.springbootazuredemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootAzureDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
